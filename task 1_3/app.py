9fb7d3d99a627053f7fa67015cd4d8fefc2f5271
Artem Druzhinin <tyomafrutis@mail.ru>
2026-03-01 00:05:10 +0300

    task 1.4

--- task 1_3/app.py
+++ task 1_3/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import User
 
 app = FastAPI()
 
@@ -6,3 +7,9 @@ app = FastAPI()
 def calculate(num1: int, num2: int):
     result = num1 + num2
     return {"result": result}
+
+
+user: User = User(name="Druzhinin Artem", id=1)
+@app.get("/users")
+def userInfo():
+    return {"name": user.name, "id": user.id}
