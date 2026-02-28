307e6cf3d9ae69dcfd8a4025f660be0773598a0f
Artem Druzhinin <tyomafrutis@mail.ru>
2026-03-01 00:47:19 +0300

    task 2.2

--- task 1_5/app.py
+++ task 1_5/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-from models import User
+from models import User, Feedback
 
 app = FastAPI()
 
 @app.post("/user")
 def userInfo(user: User):
     return {"name": user.name, "age": user.age, "is_adult": user.age >= 18}
+
+feedbacks = []
+@app.post("/feedback")
+def addFeedback(feedback: Feedback):
+    if ("всё плохо" in feedback.message.lower()):
+        return "Ошибка добавления отзыва"
+    feedbacks.append(feedback)
+    return {"message": f"Спасибо, {feedback.name}! Ваш отзыв сохранён."}
--- task 1_5/models.py
+++ task 1_5/models.py
@@ -1,6 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 class User(BaseModel):
     name: str
     age: int
     
+class Feedback(BaseModel):
+    name: str = Field(..., min_length=2,max_length=50)
+    message: str = Field(..., min_length=10, max_length=500)
+
+    @validator('message')
+    def false_message(cls, value):
+        bad_words = ["кринж", "рофл", "вайб"]
+        for word in bad_words:
+            if word in value.lower():
+                raise ValueError(f"Использование недопустимых слов")
+        return value
+        
